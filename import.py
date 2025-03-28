5cb21c086c0c13d9ffabe6c233d2284e6fcb4430
ASHISH KUMAR <150271551+Ashishkumar1198@users.noreply.github.com>
2025-03-28 13:36:10 +0530

    Create import.py
    This is the libraries to import to run this project

---
+++ import.py
@@ -0,0 +1,12 @@
+import os
+import tkinter as tk
+from tkinter import filedialog,ttk, messagebox, simpledialog
+import ctypes
+import sys
+import shutil
+import stat
+import send2trash
+import pythoncom
+import subprocess
+import threading
+import win32com.client
